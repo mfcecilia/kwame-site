26bddcd330a3dd5e8175e1c03be82e59fe357009
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2017-06-10 11:42:37 -0600

    0011 - google app script, figuring out excel parsing, alpha modal debugging

--- contractors.js
+++ contractors.js
@@ -5,7 +5,7 @@
 //get data from excel and use for html output.
 
 //get the api key for to access the contractor doc
-var sheet = "https://spreadsheets.google.com/feeds/cells/AIzaSyCt0pkxd9kwF1a0vuG6jLgstdq81FGgPXI/1/public/values?alt=json-in-script&callback=";
+var sheetUrl = "https://spreadsheets.google.com/feeds/cells/AIzaSyCt0pkxd9kwF1a0vuG6jLgstdq81FGgPXI/1/public/values?alt=json-in-script&callback=";
 
 //variable to hold the object that grabs data from a url
 var request = new XMLHttpRequest();
@@ -20,8 +20,16 @@ var info = document.getElementById("alpha-list");
 
 function getExcelData() {
     'use strict';
-    request.open('GET', sheet, true);
-    request.send();
+    console.log("START getExcelData");
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            info.innerHTML = request.responseText;
+            console.log("...displaying data...");
+        }
+    };
+    request.open("GET", sheetUrl, true);
+    request.send(null);
+    console.log("END getExcelData");
 }
 
 
--- job-search.js
+++ job-search.js
@@ -1,57 +1,80 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define */
-
-//alphabetical search
-
+/* global define */
+/* jslint latedef:false*/
 
 
-// get the modal
-var modal = document.getElementById('jobs-alpha-modal');
+/*
+* global variables
+*/
+
+//check if user clicks at all
+var clicked = document.addEventListener("click", window);
+
+
+
+/*
+*
+*
+*alphabetical search
+*
+*
+*/
+
+
 
 // get the search button that opens the modal
-var btn = document.getElementById("jobs-alpha-btn");
+var jobsAlphaBtn = document.getElementById("jobs-alpha-btn");
+
+//get modal for alphabetical job search
+var jobsAlphaModal = document.getElementById('jobs-alpha-modal');
 
 // get the <span> element that closes the modal
-var span = document.getElementsByClassName("jobs-alpha-close")[0];
-
-//check if initial search button is pressed
-var alphaBtnClick = window.onload = function () {
-    'use strict';
-    // get the search button that opens the modal
-    var btn = document.getElementById("jobs-alpha-btn");
-    btn.onclick();
-};
+var jobsAlphaClose = document.getElementsByClassName("jobs-alpha-close")[0];
 
 //check if user clicked the button to close the alpha job search modal
-var alphaCloseClick = window.onload = function () {
-    'use strict';
-    // get the <span> element that closes the modal
-    var span = document.getElementsByClassName("jobs-alpha-close")[0];
-    span.onclick();
-};
+var alphaCloseClick = jobsAlphaClose.onclick;
 
 
-// when the user clicks on the button, open the modal 
-function alphaBtn() {
+
+
+
+
+
+/*
+* when the user clicks on the alphabetical search
+* button, open the modal to show company list.
+*/
+
+function alphaBtnEvents() {
     'use strict';
-    // get the modal
-    var modal = document.getElementById('jobs-alpha-modal');
+    
+    var jobsAlphaBtn = document.getElementById("jobs-alpha-btn");
+    
+    var jobsAlphaModal = document.getElementById('jobs-alpha-modal');
+    
     //tell console we are inside the function
-    console.log("inside alphaBtn function");
+    console.log("START alphaBtnEvents function");
+    
     //check if user has clicked the alpha job search button
-    if (alphaBtnClick) {
+    if (jobsAlphaBtn.addEventListener("click", jobsAlphaBtn) !== null) {
         //tell console we registered the button click and ready to proceed
-        console.log("inside the alphabetical job search button click check");
+        console.log("...alphabetical job search button click registered....");
+        
+        //say what the user clicked
+        
+        
         //display the modal
-        modal.style.display = "block";
+        jobsAlphaModal.style.display = "block";
+
         //opaque backdrop displayed
-        document.getElementById("main-page-content").style.opacity = "0.5";
+
         //tell console that all actions have completed successfully
         console.log("the alphabetical job search button click actions are complete");
     } else {
-        console.log("the alphaBtn function to open the modal has failed");
+        console.log("alphaBtn function to open the modal FAILED");
     }
 }
+
 
 // when the user clicks on <span> (x), close the modal
 function alphaClose() {
@@ -59,11 +82,12 @@ function alphaClose() {
     // get the modal
     var modal = document.getElementById('jobs-alpha-modal');
     console.log("inside alphaClose function");
-    if (alphaCloseClick) {
+    if (alphaCloseClick !== null) {
         console.log("inside the close alpha job search button click check");
-        modal.style.display = "none";
+        jobsAlphaModal.style.display = "none";
+        
         //opaque backdrop displayed
-        document.getElementById("main-page-content").style.opacity = "1";
+        
         console.log("the close alpha job search button click was registered");
     } else {
         console.log("the alphaClose function to close the modal has failed");
@@ -73,8 +97,10 @@ function alphaClose() {
 // when the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     'use strict';
-    if (event.target === modal) {
-        modal.style.display = "none";
+    // get the modal
+
+    if (clicked === jobsAlphaModal) {
+        jobsAlphaModal.style.display = "none";
     }
 };
 
@@ -87,4 +113,5 @@ window.onclick = function (event) {
 
 //amount of contacts search
 
-
+// get the modal
+var modal = document.getElementById('jobs-amt-modal');
