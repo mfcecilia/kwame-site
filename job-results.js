81b027060e7fa460423bd03200ed7bd932fbfbf5
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2017-06-18 13:02:20 -0600

    0019 - clear alpha results function now works; starting tooltips

--- job-results.js
+++ job-results.js
@@ -4,144 +4,53 @@
 
 
 
+
 /*
 *
 *
-*job search results javascript mechanics
+*remove deselected items from the result view
 *
 *
 */
 
-
-/* globals to track user history*/
-var prevAlphaSelection;
-
-/*
-*
-when the user selects companies, they appear as a preview to review before purchasing.
-as the user selects and deselects companies, the results will modify to reflect the changes.
-*
-*/
-
-function getAlphaChecks() {
+function clrAlphaResults() {
     'use strict';
+    console.log("START clrAlphaResults");
     
-    //VARS
-    var a = 0;
-    var i = 0;
+    //store reset header
+    var jobsAlphaReset = document.getElementsByClassName("alpha-modal-header")[0];
+    //store checkbox class
+    var alphaXboxes = document.getElementsByClassName('alpha-checkbox');
+    //results elements
+    var resultView = document.getElementsByClassName('result-view');
+    var resultItem = document.getElementsByClassName('result-item');
     
-    //RESULT ARRAY
-    var resultItems = document.getElementsByClassName('result-view');
-    var resultFind = document.getElementsByClassName("result-view")[a];
-    var getResults = resultFind.getElementsByTagName("br");
-    var resultChild = resultItems[a].getElementsByClassName('result-view')[a];
-    var resultDivId = document.getElementById('result-preview');
-    var resultTag = document.createElement("br");
-    var resultObj = resultItems[a];
-    
-    //CHECKBOX ARRAY
-    var alphaChecks = document.getElementsByClassName('alpha-checkbox');
-    var alphaCheckbox = alphaChecks[i]; //no return value
-    var checkedValue = alphaCheckbox.checked; //true or false
-    var currAlphaCheckVal = alphaCheckbox.value; //string
-    
-    
-    //BEGIN ANALYZING
-    console.log("START getAlphaChecks function");
-
-    //CHECKBOX FOR LOOP
-    for (i = 0; alphaChecks[i]; ++i) {
+    //on reset button click
+    if (jobsAlphaReset.addEventListener("click", jobsAlphaReset) !== null) {
+        console.log("...clear alpha job search results button click registered...");
         
-        console.log("...inside alphaCheckbox FOR loop... " + i);
-
-        console.log("...now analyzing current checkbox: " + currAlphaCheckVal);
+        var j = 0;
         
-        /*RESULT VIEW IS EMPTY*/
-        if (resultDivId.innerHTML === "") {
-            console.log("...result view is empty... : " + resultDivId.innerHTML);
-            /*CURRENT BOX CHECKED*/
-            if (checkedValue) {
-                console.log(currAlphaCheckVal + " is selected...(current checkbox value)...");
+        //iterate over checkboxes
+        for (j; j < alphaXboxes.length; j++) {
+            if (alphaXboxes[j].checked === false) {
+                console.log("analyzing unchecked alpha value: " + alphaXboxes[j].value);
                 
-                console.log("...ADDING TO RESULT view...: " + currAlphaCheckVal);
-                var newText = document.createTextNode(currAlphaCheckVal);
-                resultTag = resultTag.appendChild(newText);//append txt node
-                console.log("resultTag = ", resultTag); //check output
-                
-                resultDivId.appendChild(resultTag);//add to result view
-                
-                
-                prevAlphaSelection = alphaChecks[i].value;
-                console.log("...recording user selection history: " + prevAlphaSelection + " ...added...");
-                
-                console.log("results now include: " + resultDivId.innerHTML);
-                console.log("...this current alphaCheckbox loop #: " + i + " is now ending...");
-                /*CURRENT BOX UNCHECKED*/
-            } else {
-                console.log("...result view empty AND current box is unchecked...nothing to do here...");
-                console.log("...this current alphaCheckbox loop #: " + i + " is now ending...");
-            }
-            
-        /*RESULT VIEW IS NOT EMPTY*/
-        } else if (resultDivId.innerHTML !== "") {
-            console.log("...result view is not empty... : " + resultDivId.innerHTML);
-            
-            
-            //RESULT VIEW FOR LOOP
-            for (a = 0; resultItems[a]; a++) {
-                console.log("...inside resultItems FOR loop... " + a);
-                console.log("...now analyzing this resultObj value: " + resultItems[a].innerHTML);
-                
-                /*CURRENT CHECKBOX CHECKED*/
-                /*CURRENT CHECKBOX EQUALS RESULT VAL*/
-                if (checkedValue && (currAlphaCheckVal === resultItems[a].innerHTML)) {
-                    //do nothing
-                    console.log("...cannot add currAlphaCheckVal: " + currAlphaCheckVal + " if resultVal exists: " + resultItems[a].innerHTML);
-                    console.log("...this current resultItems loop #: " + a + " is now ending...");
-                    
-                /*CURRENT CHECKBOX UNCHECKED*/
-                /*CURRENT CHECKBOX EQUALS RESULT VAL*/
-                } else if (!checkedValue && (currAlphaCheckVal === resultItems[a].innerHTML)) {
-                    //remove it
-                    console.log("...removing resultVal " + resultItems[a].innerHTML + " because currAlphaCheckVal " + currAlphaCheckVal + " is not checked...");
-                    resultDivId.removeChild(resultItems[a]);
-                    console.log("...this current resultItems loop #: " + a + " is now ending...");
-                } else {
-                    console.log("not sure what other checks i need, so I'm ADDING currAlphaCheckVal: " + currAlphaCheckVal + " to result view....");
-                    resultTag.innerHTML = currAlphaCheckVal;
-                    resultDivId.appendChild(resultTag);
+                var b = 0;
+
+                //iterate over results and delete matches
+                for (b; b < resultItem.length; b++) {
+                    if (alphaXboxes[j].value === resultItem[b].innerHTML) {
+                        console.log("found match, now removing...: " + resultItem[b].innerHTML);
+                        document.getElementById('result-preview').removeChild(resultItem[b]);
+                    }
                 }
-                console.log("...this current resultItems loop #: " + a + " is now ending...");
             }
-            console.log("EXITING resultObj FOR loop");
-            //might need else if for results empty and box unchecked
-        } else {
-            console.log("something might be wrong inside getAlphChecks() function OR i just don't have all the right if statements");
         }
-        console.log("...this current alphaChecks loop #: " + a + " is now ending...");
         
-    }
-    
-    console.log("EXITING alphaChecks loop...getAlphaChecks FUNCTION ENDING...");
-    
-    
-    /*summary of the function events*/
-    if (resultDivId !== "") {
-        console.log("...function ending number of items in result preview: " + resultDivId.childElementCount);
-        console.log("...function ending result preview contents: " + resultDivId.innerHTML);
     } else {
-        console.log("...function ending... nothing in result view");
+        console.log("clrAlphaResults button click not registered");
     }
     
-    
-    if (checkedValue) {
-        console.log("...function ending... alphaChecks[i].value is currently checked: " + currAlphaCheckVal);
-    } else {
-        console.log("...function ending... done analyzing the unchecked alphaChecks[i] value: " + currAlphaCheckVal);
-        console.log("...function ending... previously acted upon by user: " + prevAlphaSelection);
-    }
-    
-    console.log("END getAlphaChecks");
+    //note to user that their items were removed from the results
 }
-
-/*function getAlphaUnchecks()*/
--- job-search.js
+++ job-search.js
@@ -126,6 +126,8 @@ function alphaClose() {
         console.log("alphaClose function to close the modal FAILED");
     }
     console.log("END alphaClose");
+    
+    //tooltip: click here to exit without losing your changes
 }
 
 
@@ -141,14 +143,14 @@ function alphaSave() {
     console.log("START alphaSave function");
     
     var jobsAlphaModal = document.getElementById('jobs-alpha-modal');
-    var alphaModalFooter = document.getElementById('alpha-modal-footer');
+    var alphaModalFooter = document.getElementsByClassName('alpha-modal-footer');
     var alphaXboxes = document.getElementsByClassName('alpha-checkbox');
     var resultView = document.getElementsByClassName('result-view');
     var resultItem = document.getElementsByClassName('result-item');
     
     //on click
     if (window.addEventListener("click", alphaModalFooter) !== null) {
-        console.log("...done button in alpha modal registered...");
+        console.log("...save button in alpha modal registered...");
         
         //checkbox iterator
         var a = 0;
@@ -167,7 +169,6 @@ function alphaSave() {
                 console.log("alphaX[a]: " + alphaX[a]);
             }
         }
-        console.log("amount selected in alpha search: " + alphaX.length);
         
         var b = 0;
         var newResult;
@@ -278,23 +279,105 @@ function amtClose() {
 
 /*
 *
-when user clicks "done at the end of the alphabetical company search modal, close the modal
+when user clicks "done at the end of the alphabetical company search modal, take a snapshot of selections, reveal them in the preview, and close the modal
 *
 */
 
-function amtDone() {
+function amtSave() {
     'use strict';
     
-    console.log("START amtDone function");
+    console.log("START amtSave function");
     
     var jobsAmtModal = document.getElementById('jobs-amt-modal');
-    var amtModalFooter = document.getElementById('amt-modal-footer');
+    var amtModalFooter = document.getElementsByClassName('amt-modal-footer');
+    var amtXboxes = document.getElementsByClassName('amt-checkbox');
+    var resultView = document.getElementsByClassName('result-view');
+    var resultItem = document.getElementsByClassName('result-item');
     
+    //on click
     if (window.addEventListener("click", amtModalFooter) !== null) {
-        console.log("...done button in amt of contacts modal registered...");
+        console.log("...save button in amt modal registered...");
+        
+        //checkbox iterator
+        var a = 0;
+        //array of selected items
+        var amtX = [];
+        
+        //gather selections
+        for (a; a < amtXboxes.length; a++) {
+            console.log("analyzing checkbox #: " + a);
+            if (amtXboxes[a].checked) {
+                amtX[a] = (amtXboxes[a].value);
+                console.log("selected in amt search: " + amtXboxes[a].value);
+                console.log("added: " + amtX[a]);
+            } else {
+                console.log("not checked");
+                console.log("amtX[a]: " + amtX[a]);
+            }
+        }
+        
+        var b = 0;
+        var newResult;
+        
+        //populate preview
+        for (b; b < amtX.length; b++) {
+            if (typeof amtX[b] === 'string' || amtX[b] instanceof String) {
+                console.log("adding to result view: " + amtX[b]);
+                newResult = document.createElement('div');
+                newResult.className = 'result-item';
+                document.getElementById('result-preview').appendChild(newResult);
+                resultItem[b].innerHTML = amtX[b];
+                console.log("result now contains: " + resultItem[b].innerHTML);
+            }
+        }
+        
+        //close modal
         jobsAmtModal.style.display = "none";
     } else {
-        console.log("amtDone function FAILED");
+        console.log("amtSave function FAILED");
     }
-    console.log("END amtDone");
+    console.log("END amtSave");
+}
+
+
+/*
+*
+when the user clicks on 'reset', uncheck everything
+*
+*/
+
+function amtReset() {
+    'use strict';
+    
+    //tell console we are inside the function
+    console.log("START amtReset function");
+    
+    // store the modal element
+    var jobsAmtModal = document.getElementById('jobs-amt-modal');
+    // store the <span> element that closes the modal
+    var jobsAmtReset = document.getElementsByClassName("amt-modal-header")[0];
+    //store amt checkbox class
+    var amtXboxes = document.getElementsByClassName('amt-checkbox');
+    
+    //on click
+    if (jobsAmtReset.addEventListener("click", jobsAmtReset) !== null) {
+        
+        
+        var j = 0;
+        
+        //find and uncheck checked items
+        for (j; j < amtXboxes.length; j++) {
+            if (amtXboxes[j].checked) {
+                amtXboxes[j].checked = false;
+                console.log("deleted in amt search: " + amtXboxes[j].value);
+            }
+        }
+        
+        //clear amt search items from result preview
+        
+        console.log("...reset amt job search button click registered...");
+    } else {
+        console.log("amtReset function to close the modal FAILED");
+    }
+    console.log("END amtReset");
 }
