05b7742d3e705a15c91ba3a15ede70f76237e2a4
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2017-06-18 21:34:13 -0600

    0025 - enabled scrolling in alpha modal

--- contractors.js
+++ contractors.js
@@ -7,23 +7,10 @@
 /*
 *
 *
-*XML http request
+*XML http request for alpha search
 *
 *
 */
-
-function loadAlphaXML() {
-    'use strict';
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            alphaContractors(this);
-            console.log("loading xml doc");
-        }
-    };
-    xmlhttp.open("GET", "/files/contractors.xml", true);
-    xmlhttp.send();
-}
 
 function alphaContractors(xml) {
     'use strict';
@@ -39,3 +26,59 @@ function alphaContractors(xml) {
     console.log("x.length = " + x.length);
     document.getElementById("alpha-list-container").innerHTML = txt;
 }
+
+
+
+function loadAlphaXML() {
+    'use strict';
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            alphaContractors(this);
+            console.log("loading xml doc");
+        }
+    };
+    xmlhttp.open("GET", "/files/contractors.xml", true);
+    xmlhttp.send();
+}
+
+
+
+
+/*
+*
+*
+*XML http request for amt search
+*
+*
+*/
+
+function amtContractors(xml) {
+    'use strict';
+    var x, i, xmlDoc, txt;
+    xmlDoc = xml.responseXML;
+    txt = "";
+    x = xmlDoc.getElementsByTagName("Worksheet")[1].getElementsByTagName("Row");
+
+    
+    for (i = 0; i < x.length; i++) {
+        txt += x[i].childNodes[1].textContent + "<br>";
+    }
+    console.log("x.length = " + x.length);
+    document.getElementById("alpha-list-container").innerHTML = txt;
+}
+
+
+function loadAmtXML() {
+    'use strict';
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            amtContractors(this);
+            console.log("loading xml doc");
+        }
+    };
+    xmlhttp.open("GET", "/files/contractors.xml", true);
+    xmlhttp.send();
+}
+
