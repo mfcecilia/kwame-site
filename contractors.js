8cdc017c1ef79328ae56ee46fe7a0ffda8c88e49
mfcecilia <mkaykay1@vt.edu>
2017-06-27 17:08:44 -0600

    0033 - start counting number of contacts using numContacts()

--- contractors.js
+++ contractors.js
@@ -62,23 +62,28 @@ function loadAlphaXML() {
 
 function amtContractors(xml) {
     'use strict';
-    var x, numContacts, i, xmlDoc, txt; //need to figure out numContacts
-    var contactCount; //2d array to track company and number of duplicates (contacts)
+    var x, y, i, xmlDoc, txt;
     xmlDoc = xml.responseXML;
     txt = "";
+    
     //variable to store "row" tags
     x = xmlDoc.getElementsByTagName("Worksheet")[0].getElementsByTagName("Row");
 
     //iterate each row
     for (i = 1; i < x.length; i++) {
         if (x[i].childElementCount === 6) {
+            y = x[i].getElementsByTagName("Cell")[4].childNodes[0].textContent + "<br>";
+            //numContacts(y, x.length, i)
             console.log("row child count = " + x[i].childElementCount);
             console.log("adding.... " + x[i].getElementsByTagName("Cell")[4].childNodes[0].textContent);
             txt += x[i].getElementsByTagName("Cell")[4].childNodes[0].textContent + "<br>";
         } else if (x[i].childElementCount === 5) {
+            y = x[i].getElementsByTagName("Cell")[3].childNodes[0].textContent + "<br>";
+            //numContacts(y, x.length, i)
             console.log("row child count = " + x[i].childElementCount);
             console.log("adding.... " + x[i].getElementsByTagName("Cell")[3].childNodes[0].textContent);
             txt += x[i].getElementsByTagName("Cell")[3].childNodes[0].textContent + "<br>";
+            
         } else {
             console.log("could not add child node");
         }
@@ -88,6 +93,52 @@ function amtContractors(xml) {
 }
 
 
+
+
+/*
+*
+*
+*count number of contacts each company has
+*
+*
+*/
+
+function numContacts(num, amt, count) {
+    'use strict';
+    
+    //num represents the current row item being analyzed
+    //amt represents x.length
+    //count represents i
+    
+    var a; //arrAmt row iterator
+    var b; //arrAmt col iterator
+    var c; //zero
+    
+    //2d array to count the amount of contacts per company
+    var arrAmt = [];
+    
+    //only check if there are already items have have been added
+    if (count > 1) {
+        for (a = 0; a < arrAmt.length; a++) {
+            //check if the current item is already in arrAmt[][]
+            if (arrAmt[a][b] === num) {
+                //check if there are multiple columns already
+                if (arrAmt[a] > 0) {
+                //find the next empty column
+                //or avoid this check and see if there's a push method to add to beginning instead of end
+                //add to column in arrAmt
+                } else {
+                    //idk
+                }
+            } else {
+                //idk
+            }
+        }
+    } else {
+        //add current item being analyzed to arrAmt[i][a] if it is the first in the xml
+        arrAmt[count][c] = num;
+    }
+}
 
 /*
 *
@@ -109,4 +160,3 @@ function loadAmtXML() {
     xmlhttp.open("GET", "/files/contractors.xml", true);
     xmlhttp.send();
 }
-
