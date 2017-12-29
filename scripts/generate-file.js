c8a5c852ca0f4aaabd957faa104cebe8f2d77f5d
mfcecilia <mkaykay1@vt.edu>
2017-12-29 00:49:54 -0700

    reorganize repo, add comments

---
+++ scripts/generate-file.js
@@ -0,0 +1,28 @@
+/*
+Upon clicking "Purchase", the list of items that are left in the result preview are 
+put into an array.
+*/
+function getSelections() {
+    'use strict';
+    //iterate over all result item id's in result preview
+    //if they are style.display block, add to the array
+}
+
+
+/*
+Using the array of final selections, their contacts are found and 
+paired up via a more sophisticated data structure.
+*/
+function matchContacts() {
+    'use strict';
+    
+}
+
+
+/*
+Parse the data into a well formatted file
+*/
+function generateFile() {
+    'use strict';
+    
+}
